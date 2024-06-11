796b14551efaf6dc0241553143c366796e8721ef
Roberto Moreira <robertomrr@yahoo.com.br>
2024-06-11 14:56:16 -0300

    Instalação do livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\CounterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,7 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/counter', CounterController::class);
 
 require __DIR__.'/auth.php';
